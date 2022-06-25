e4b0cc92f482a385502ebd681686bae75813f60c
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-25 16:56:14 -0700

    initial documentation

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 //jshint esversion:9
 
-// 
+// ****************************************************************
+//
+//      APP DESCR:
+//
+//      watchit is a nodemon clone (without server) that watches 
+//      for changes in your app and then reports these changes to you
+//      through an active process in a CLI.
+//
+// ****************************************************************
+
