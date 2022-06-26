70c95bebbeeff9a8c00f3eeb642acb5f03e89526
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-25 23:03:26 -0700

    refactor debounce and watcher

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
 //      watchit is a nodemon clone (without server) that watches 
 //      for changes in your app and then reports these changes to you
 //      through an active process in a CLI.
+//      
+//      Run in terminal using 'watchit'
 //
 // ****************************************************************
 
@@ -37,27 +39,35 @@ const program = require('caporal');
 // ****************************************************************
 
 // Initialize caporal and configure CLI
+//      square brackets indicate arg is optional
+//`     angle brackets indicate arg is necessary
 program
     .version('0.0.1') // set a flag to tell the user the version
     .argument('[filename]', 'Name of a file to execute') // specify the arguments a command will take
-    .action( (args) => console.log(args) ); // runs a function that accepts args, options, etc.
+    .action( // runs a function that accepts args, options, etc
+        (args) => {
+            // 
+            watchAndListen();
+    }); 
 
 // Run the caporal configuration program
 program.parse(process.argv);
 
-// Debounce is a wrapper that will stop a function from being called too often
-// by returning a new version of the function that cannot be called too often
-const start = debounce(() => {
-    console.log('Starting User Program');
-}, 100); // debounce 100 milliseconds
+const watchAndListen = function() {
+    // Debounce is a wrapper that will stop a function from being called too often
+    // by returning a new version of the function that cannot be called too often
+    const start = debounce((input) => {
+        console.log('${input} detected');
+    }, 100); // debounce 100 milliseconds
 
-// Initialize Watcher
-//      Watch from current directory
-//      Attach 3 event listeners and handlers
-//  vv
-chokidar
-    .watch('.')
-    .on('add', () => start()) // listen for add, log occurrence
-    .on('change', () => console.log('file changed')) // listen for change, log occurrence
-    .on('unlink', () => console.log('file unlinked')); // listen for unlink, log occurrence
+    // Initialize Watcher
+    //      Watch from current directory
+    //      Attach 3 event listeners and handlers
+    //  vv
+    chokidar
+        .watch('.')
+        .on('add', () => start('add')) // listen for add, log occurrence
+        .on('change', () => start('change')) // listen for change, log occurrence
+        .on('unlink', () => start('unlink')); // listen for unlink, log occurrence
+};
 
